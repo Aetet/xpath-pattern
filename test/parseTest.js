b4a11d0cfbfca8a52ff100a3875e012b0e388520
Aetetic <aetetic@gmail.com>
2014-04-19 21:31:33 +0400

    Add tests and implementation for validation pattern

--- test/parseTest.js
+++ test/parseTest.js
@@ -1,6 +1,28 @@
 var chai = require('chai');
 var expect = chai.expect;
-describe('Hello parser', function () {
-  it('Hello', function () {
+var should = chai.should();
+var xpathPattern = require('../lib/xpath-pattern');
+describe('XPathParser', function () {
+  describe('#.isValidPattern', function () {
+    it('double <<', function () {
+      xpathPattern.isValidPattern('/asdf/sd/<sdfjdfk<fkdjf>>')
+        .should.be.eql.false;
+    });
+    it('double >>', function () {
+      xpathPattern.isValidPattern('/asdfkjasdf/<sdfkjf>fdskjf>')
+        .should.be.eql.false;
+    });
+    it('unclosed <', function () {
+      xpathPattern.isValidPattern('/fksdjfasdfkjsdf<')
+        .should.be.eql.false;
+    });
+    it('close without open angle', function () {
+      xpathPattern.isValidPattern('/asdfkajsf<fskdfjf>aksdfkjsdf>')
+        .should.be.eql.false;
+    });
+    it('valid expression', function () {
+      xpathPattern.isValidPattern('/adfkasdfj<sdfksdfjsdf>ssdfksdfj<ssdkfj><sdlfkjsdf><ksdjf>')
+        .should.be.eql.true;
+    });
   });
 });
