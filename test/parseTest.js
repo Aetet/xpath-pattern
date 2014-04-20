07a1cdf8d30639b1bb1de3a1442af59d0a10e797
Aetetic <aetetic@gmail.com>
2014-04-20 12:30:19 +0400

    fix tests for valid pattern

--- test/parseTest.js
+++ test/parseTest.js
@@ -5,24 +5,29 @@ var xpathPattern = require('../lib/xpath-pattern');
 describe('XPathParser', function () {
   describe('#.isValidPattern', function () {
     it('double <<', function () {
+
       xpathPattern.isValidPattern('/asdf/sd/<sdfjdfk<fkdjf>>')
-        .should.be.eql.false;
+        .should.be.false;
     });
     it('double >>', function () {
       xpathPattern.isValidPattern('/asdfkjasdf/<sdfkjf>fdskjf>')
-        .should.be.eql.false;
+        .should.be.false;
     });
     it('unclosed <', function () {
       xpathPattern.isValidPattern('/fksdjfasdfkjsdf<')
-        .should.be.eql.false;
+        .should.be.false;
     });
     it('close without open angle', function () {
       xpathPattern.isValidPattern('/asdfkajsf<fskdfjf>aksdfkjsdf>')
-        .should.be.eql.false;
+        .should.be.false;
     });
     it('valid expression', function () {
       xpathPattern.isValidPattern('/adfkasdfj<sdfksdfjsdf>ssdfksdfj<ssdkfj><sdlfkjsdf><ksdjf>')
-        .should.be.eql.true;
+        .should.be.true;
+    });
+    it('another valid expression', function () {
+      xpathPattern.isValidPattern('/a/<foo>/dkfj/<mooochaha>/<mio>')
+        .should.be.true;
     });
   });
 });
